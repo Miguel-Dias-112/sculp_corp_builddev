9580f67c193b0ea89d2891e3975a68c3e7ea5e9f
MiguelDias <miguel.dias@estudante.ufjf.br>
2024-06-25 00:34:40 -0300

    hot fix

--- vite.config.js
+++ vite.config.js
@@ -25,19 +25,25 @@ export default defineConfig({
         page14: './Paginas/supercha/index.html', // página inicial
 
         page3: './Paginas/defictcalorico/index.html', // página inicial
-        page16: './Paginas/defictcalorico/ajustestempo/index.html', // página inicial
+        page16: './Paginas/defictcalorico/ajustetempo/index.html', // página inicial
         page17: './Paginas/defictcalorico/comousar/index.html', // página inicial
-        page18: './Paginas/defictcalorico/descubraseu/index.html', // página inicial
-        page19: './Paginas/defictcalorico/entendendodeficit/index.html', // página inicial
+        page18: './Paginas/defictcalorico/descubraoseu/index.html', // página inicial
+        page19: './Paginas/defictcalorico/entendendodefict/index.html', // página inicial
         page20: './Paginas/defictcalorico/monitoramento/index.html', // página inicial
 
         page21: './Paginas/exercicio/mulheres/index.html', // página inicial
-        page22: './Paginas/exercicio/treinos/peitoTricepsAbdomen/index.html', // página inicial
-        page23: './Paginas/exercicio/treinos/pernaPanturrilha/index.html', // página inicial
+        page22: './Paginas/exercicio/treinos/peitoTricepsAbdomen/avancado.html', // página inicial
+        page27: './Paginas/exercicio/treinos/peitoTricepsAbdomen/iniciante.html', // página inicial
+        page28: './Paginas/exercicio/treinos/peitoTricepsAbdomen/intermediario.html', // página inicial
+
+
+        page23: './Paginas/exercicio/treinos/pernaPanturrilha/iniciante.html', // página inicial
+        page29: './Paginas/exercicio/treinos/pernaPanturrilha/intermediario.html', // página inicial
+        page30: './Paginas/exercicio/treinos/pernaPanturrilha/avancado.html', // página inicial
         page24: './Paginas/exercicio/index.html', // página inicial
 
         page25: './Paginas/listacompras/index.html', // página inicial
-        page25: './Paginas/tabelaalimentos/index.html', // página inicial
+        page26: './Paginas/tabeladealimentos/index.html', // página inicial
 
 
       },
